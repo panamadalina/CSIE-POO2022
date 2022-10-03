91f69b9f531560982135da488e6aeb133817ff09
Madalina Pana <madalina.pana1997@gmail.com>
2022-10-03 23:58:54 +0300

    seminar2-exemplu1

---
+++ seminar2-exemplu1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int Main()
+{
+    int x = 10;
+    int y = 15;
+
+    cout << " " << (x, y)
+
+        while (!)
+        {
+            cout << "hello";
+        }
+
+    int n = 9, div = 0;
+    div = n / 0;   cout << "result = " << div;
+
+    for (int i = 0; i < 3; i++);
+    {
+        cout << "loop ";
+        continue;
+
+    }
+
+}
