1b097c8beb43aae6d954606532b8b2c32eedf32f
Madalina Pana <madalina.pana1997@gmail.com>
2022-10-25 01:03:30 +0300

    no message

---
+++ seminar4_2.cpp
@@ -0,0 +1,120 @@
+#include<iostream>
+#include<stdio.h>
+#include <crtdbg.h>
+#include<string>
+using namespace std;
+
+
+
+struct Pers {
+	int varsta;
+};
+
+//clasa = user defined datatype (un tip de obiecte)
+//Membrii unei clase: atribute & metode 
+
+class Persoana {
+
+	//atribut fara sa specifam tip de accesare este PRIVATE
+	int varsta;
+
+protected:
+	bool lucreaza;
+
+public:
+	int idPersoana;
+	char* nume;
+
+	
+	void metoda1() {
+		cout << "\nMetoda1";
+	}
+
+
+	~Persoana() {
+		if (this->nume != NULL) {
+			delete[] this->nume;
+			this->nume = nullptr;
+		}
+		//_CrtDumpMemoryLeaks();//nu merge
+	}
+};
+
+//derivare/specializarea tipurilor de obiecte
+
+class Angajat :Persoana {
+
+	//Persoana p;
+	int salariu;
+public:
+
+	void method1() {
+		this->idPersoana = 0;
+		this->lucreaza = true; //protected poate fi accesat
+	}
+
+	int getSalariu(int codAcces) {
+		if (codAcces == 1234) {//caz ipotetic
+			return salariu;
+		}
+		else {
+			return -1;
+		}
+	}
+
+	void setSalariu(int codAcces, int salariuNou) {
+		if (codAcces == 1234) {//caz ipotetic
+			salariu = salariuNou;
+		}
+		else {
+			//alerta
+		}
+	}
+};
+
+
+int main() {
+	//Ce este o clasa?
+	// 	   Ce contine o clasa?
+
+	
+	//	Pers pers;
+	Persoana p; //acesta e un obiect -> o instanta a unei clase
+	//unde e salvat acest obiect?
+	
+	//Avem 3 tipuri de accesari:
+
+	/*
+	public - members are accessible from outside the class
+	private (BY DEFaULT) - members cannot be accessed (or viewed) from outside the class --->> INCAPSULARE!!
+	protected - members cannot be accessed from outside the class, however, they can be accessed in inherited classes. You will learn more about Inheritance later.*/
+
+	p.idPersoana = 1234;
+	p.nume = new char[5];
+	strcpy(p.nume, "Mada");
+	cout << "\nPersoana: " << p.idPersoana<<" are numele: "<<p.nume;
+	
+
+	p.metoda1();
+	//++ Tipuri de metode -> din clasa, din afara clasei
+
+	//GETTERI
+	cout << "\n\nGETTERI:";
+	Angajat ang;
+	cout<< " are salariul: " << ang.getSalariu(1235);//acces interzis->alerta
+
+	//SETTERI
+	cout << "\n\nSETTERI:";
+	ang.setSalariu(1235, 2000);//acces interzis->alerta
+
+	Persoana* pp1 = &p;
+	Persoana* pp2 = new Persoana();
+	////pp este un pointer la un obiect de tip Student
+
+	delete pp2;
+	pp2 = nullptr;
+
+	//EXEMPLU: destructor
+	//_CrtDumpMemoryLeaks();//nu merge
+	return 0;
+}
