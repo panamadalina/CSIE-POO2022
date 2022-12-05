1420d051e124273843b89b26766c46ff1d3a026f
Madalina Pana <madalina.pana1997@gmail.com>
2022-12-06 01:30:23 +0200

    no message

--- seminar10.cpp
+++ seminar10.cpp
@@ -88,8 +88,56 @@ ostream& operator<<(ostream& out, Persoana p)
 	return out;
 }
 
+class Student :Persoana {
+
+private:
+	int bursa;
+	char* facultate;
+
+public:
+	Student() {//Student():Persoana()
+		this->bursa = 0;
+		this->facultate = new char[strlen("necunoscut") + 1];
+		strcpy(this->facultate, "necunoscut");
+		cout << endl << "Apel constr fara param Student " << this->facultate;
+	}
+	Student(const char* nume, int v, int b, const char* fac) :Persoana(nume, v) {
+		this->bursa = b;
+		this->facultate = new char[strlen(fac) + 1];
+		strcpy(this->facultate, fac);
+		cout << endl << "Apel constr cu param Student " << this->facultate;
+	}
+
+	Student(const Student& s) :Persoana(s) {
+		this->bursa = s.bursa;
+		this->facultate = new char[strlen(s.facultate) + 1];
+		strcpy(this->facultate, s.facultate);
+		cout << endl << "Apel constr copiere Student " << this->facultate;
+	}
+
+	Student& operator=(const Student& s) {//:Persoana()//NU
+		Persoana::operator=(s);
+		this->bursa = s.bursa;
+		this->facultate = new char[strlen(s.facultate) + 1];
+		strcpy(this->facultate, s.facultate);
+		cout << endl << "Apel op= Student " << this->facultate;
+		return *this;
+	}
+
+	~Student() {
+		if (this->facultate != NULL) {
+			delete[] this->facultate;
+			cout << endl << "apel destructor student";
+		}
+	}
+};
+
 int main() {
 
-	Persoana p;
+	// Persoana p;
+	Student s;
+	Student s2("gigel", 20, 200, "CSIE");
+	Student s3 = s2;//const de copiere
+	s3 = s;//op=
 	return 0;
 }
